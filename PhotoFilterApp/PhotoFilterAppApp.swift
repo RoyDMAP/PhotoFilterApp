f4d97cc47451dce869b6264762fce6a457316d95
RoyDMAP <roy.dimapilis@yahoo.com>
2025-10-04 10:04:51 -0700

    Initial Commit

---
+++ PhotoFilterApp/PhotoFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFilterAppApp.swift
+//  PhotoFilterApp
+//
+//  Created by Roy Dimapilis on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
